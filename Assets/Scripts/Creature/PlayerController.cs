fd3a85561d4b4a219efef6e1748db6067cbb8eaa
Amir Ashtiani <pillowmermaid@gmail.com>
2019-04-14 21:44:09 -0400

    Initial commit

---
+++ Assets/Scripts/Creature/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {   
+    public float groundSpeed = 6.0f;
+    public float jumpForce = 3.0f;
+    public float airFriction = 0.5f;
+    public float gravity = 9.8f;
+    private float verticalVelocity;
+    
+    private Vector3 moveVector = Vector3.zero;
+    private Vector3 jumpVector = Vector3.zero;
+    private CharacterController controller;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        moveVector.x = Input.GetAxis ("Horizontal");
+        moveVector.z = Input.GetAxis ("Vertical");
+        moveVector = transform.TransformDirection(moveVector);
+        moveVector *= groundSpeed;
+        if (controller.isGrounded) {
+            if (Input.GetButton("Jump")) {
+                jumpVector = moveVector;
+                jumpVector.y = jumpForce;
+            } else {
+                jumpVector = Vector3.zero;
+            }
+        } else {
+            moveVector *= airFriction;
+            jumpVector.y -= gravity * Time.deltaTime;
+        }
+        moveVector = Vector3.ClampMagnitude(moveVector, groundSpeed);
+        controller.Move ((moveVector + jumpVector) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float lookSpeed = 3;
+    public Camera playerCamera;
+    private float verticalClamp = 20.0f;
+    private Vector2 rotation = Vector2.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x = Mathf.Clamp(rotation.x, -verticalClamp, verticalClamp);
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        playerCamera.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+    }
+}
